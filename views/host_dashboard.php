e0e66d4e7a91b94f4fbf0e340edbbc76a4649885
imadprogram <kimad7141@gmail.com>
2026-01-06 15:40:06 +0100

    fixed something in head of host dashboard

--- views/host_dashboard.php
+++ views/host_dashboard.php
@@ -20,7 +20,7 @@ $rentals = $rentControll->getRentals();
 <!DOCTYPE html>
 <html lang="en">
 
-<>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Host Dashboard - Airbnb</title>
